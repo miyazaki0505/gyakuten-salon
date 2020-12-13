91edc2bf1a270899f0eaf03c8d20aa3640896c9f
miyazaki0505 <mr.gingerman0505@gmail.com>
2020-12-13 00:48:12 +0000

    laravel_kiso4

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -2,13 +2,14 @@
 @section('content')
 
     <div class="container mt-4">
+        @foreach($posts as $post)
         <div class="card mt-4">
             <div class="card-header mb-2">
-                Title
+                {{ $post->title }}
             </div>
             <div class="card-body">
                 <p class="card-text">
-                    body
+                    {{ $post->body }}
                 </p>
             </div>
             <div class="card-footer">
@@ -17,6 +18,7 @@
                 </span>
             </div>
         </div>
+        @endforeach
     </div>
     
 @endsection('content')
